b776ef13657c5a73aeba8ba4a58f06771465f33d
martip12 <martina.pereira1209@gmail.com>
2023-08-29 17:47:55 -0300

    solucion problema click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener("click", function(){
     var miDiv = document.getElementById('elDiv');
-    miDiv.addEventListener('click', function() {
+    var boton =document.getElementById('boton')
+    miDiv.addEventListener('click', function(event) {
+        event.stopPropagation();
         alert ("Hola! Soy el div");
+    });
+    boton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        alert ("Hola! ");
     });
 });
